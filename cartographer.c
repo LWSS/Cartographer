4ff327c7c5fe5dcfc937a79298ed89ea5fbe3235
LWSS <bbchallenger100@gmail.com>
2019-01-11 17:23:58 -0800

    Fix Symbol not found on kernels with optimized suffix symbols. Added prefix to printk

--- cartographer.c
+++ cartographer.c
@@ -14,6 +14,8 @@ MODULE_DESCRIPTION("");
 MODULE_AUTHOR("");
 MODULE_LICENSE("GPL");
 
+#define cart_print(fmt, ...) printk( (KBUILD_MODNAME ": "fmt), ##__VA_ARGS__ );
+
 /* file created in /proc/ */
 #define PROC_FILENAME "cartographer"
 
@@ -59,15 +61,6 @@ static struct cart_settings settings;
 
 static int init_hook( struct ftrace_hook *hook )
 {
-    hook->address = kallsyms_lookup_name(hook->name);
-
-    if (!hook->address) {
-        printk("unresolved fn symbol: %s\n", hook->name);
-        return -ENOENT;
-    } else {
-        printk("Found fn %s at %p\n", hook->name, (void*)hook->address);
-    }
-
 #if USE_FENTRY_OFFSET
     *((unsigned long*) hook->original) = hook->address + MCOUNT_INSN_SIZE;
 #else
@@ -109,16 +102,16 @@ static asmlinkage void cart_show_map_vma(struct seq_file *m,
     unsigned long backup_flags = vma->vm_flags;
 
     if( settings.remove_entry ){ // in some cases the memory offset makes this kinda obvious, but it seems there's gaps naturally too sometimes.
-        printk("show_map_vma Hook - removing entry in maps (%s)\n", vma->vm_file->f_path.dentry->d_iname);
+        cart_print("show_map_vma Hook - removing entry in maps (%s)\n", vma->vm_file->f_path.dentry->d_iname);
         return;
     }
     if( settings.null_file ){
-        printk("show_map_vma Hook - removing file on (%s)\n", vma->vm_file->f_path.dentry->d_iname);
+        cart_print("show_map_vma Hook - removing file on (%s)\n", vma->vm_file->f_path.dentry->d_iname);
         vma->vm_file = NULL;
     }
 
     if( settings.spoof_permissions ){
-        printk("show_map_vma Hook - Setting permissions on (%s)\n", vma->vm_file->f_path.dentry->d_iname);
+        cart_print("show_map_vma Hook - Setting permissions on (%s)\n", vma->vm_file->f_path.dentry->d_iname);
         vma->vm_flags &= ~VM_READ;
         vma->vm_flags &= ~VM_WRITE;
         vma->vm_flags &= ~VM_EXEC;
@@ -142,18 +135,18 @@ static ssize_t on_write(struct file *file, const char *buf, size_t len, loff_t *
     mutex_lock(&data_mutex);
 
     if( !buf ){
-        printk("Buffer is NULL. hmmm....\n");
+        cart_print("Buffer is NULL. hmmm....\n");
         goto end;
     }
     /* strsep changes the string, buf is constant, need to copy */
     char *backup = kmalloc(len, GFP_KERNEL);
     if( !backup ){
-        printk("Failed to malloc %ld bytes for argument parsing!\n", len);
+        cart_print("Failed to malloc %ld bytes for argument parsing!\n", len);
         goto end;
     }
     err = strncpy_from_user(backup, buf, len);
     if( err < 0 ){
-        printk("Error copying arguments from userspace! (code: %d)\n", err);
+        cart_print("Error copying arguments from userspace! (code: %d)\n", err);
         goto end;
     }
     backup[len - 1] = '\0';
@@ -162,84 +155,84 @@ static ssize_t on_write(struct file *file, const char *buf, size_t len, loff_t *
     if( strstr(word, "settarget") ){
         word = strsep( &backup, " ,\n\t" );
         if( !word ){
-            printk( "settarget requires an argument! ( the target lib )\n");
+            cart_print( "settarget requires an argument! ( the target lib )\n");
             goto end;
         }
         if( strlen(word) >= 64 ){
-            printk( "target_lib max size is 64! make your name smaller or edit the source\n" );
+            cart_print( "target_lib max size is 64! make your name smaller or edit the source\n" );
             goto end;
         }
         strncpy( settings.target_lib, word, sizeof(char) * 63 );
-        printk( "target_lib set to: %s\n", settings.target_lib );
+        cart_print( "target_lib set to: %s\n", settings.target_lib );
     } else if( strstr(word, "setspoofperms") ){
         word = strsep( &backup, " ,\n\t" );
         if( !word ){
-            printk( "setspoofperms requires an argument! ( the permission #, VM_READ - 1, VM_WRITE - 2, VM_EXEC - 4 )" );
+            cart_print( "setspoofperms requires an argument! ( the permission #, VM_READ - 1, VM_WRITE - 2, VM_EXEC - 4 )" );
             goto end;
         }
         err = kstrtos8( word, 10, &res );
         if( err || ( res < 0 || res > 7 ) ){
-            printk( "setspoofperms: %s is an invalid argument! Valid is [0-7]\n", word );
+            cart_print( "setspoofperms: %s is an invalid argument! Valid is [0-7]\n", word );
             goto end;
         }
 
         settings.permissions = res;
-        printk("spoof perms set to %u\n", res);
+        cart_print("spoof perms set to %u\n", res);
     } else if( strstr(word, "nullfile") ){
         if( settings.null_file ){
-            printk("nullfile is already on! Please turn it off or reload\n");
+            cart_print("nullfile is already on! Please turn it off or reload\n");
             goto end;
         }
         if( !settings.target_lib ){
-            printk("nullfile requires target_lib to be set ( see the settarget command )\n");
+            cart_print("nullfile requires target_lib to be set ( see the settarget command )\n");
             goto end;
         }
 
         settings.null_file = true;
-        printk("nullfile Activated.\n");
+        cart_print("nullfile Activated.\n");
     } else if( strstr( word, "removeentry" ) ){
         if( settings.remove_entry ){
-            printk("removeentry is already on! Please turn it off or reload\n");
+            cart_print("removeentry is already on! Please turn it off or reload\n");
             goto end;
         }
         if( !settings.target_lib ){
-            printk("removeentry requires the target_lib to be set ( see the settarget command )\n");
+            cart_print("removeentry requires the target_lib to be set ( see the settarget command )\n");
             goto end;
         }
         settings.remove_entry = true;
-        printk("removeentry Activated.\n");
+        cart_print("removeentry Activated.\n");
     } else if( strstr( word, "spoofperms" ) ){
         if( settings.spoof_permissions ){
-            printk("spoofpermissions is already on! Please turn it off or reload\n");
+            cart_print("spoofpermissions is already on! Please turn it off or reload\n");
             goto end;
         }
 
         /* Spoof permissions can be Zero */
         settings.spoof_permissions = true;
-        printk("spoofpermissions Activated.\n");
+        cart_print("spoofpermissions Activated.\n");
     } else if( strstr( word, "turnoff" ) ){
         word = strsep( &backup, " ,\n\t" );
         if( !word ){
-            printk("turnoff requires an argument! ( the feature to turn off )\n");
+            cart_print("turnoff requires an argument! ( the feature to turn off )\n");
             goto end;
         }
         if( strstr( word, "nullfile" ) ){
-            printk("turning off nullfile.\n");
+            cart_print("turning off nullfile.\n");
             settings.null_file = false;
         } else if( strstr( word, "removeentry" ) ){
-            printk("turning off removeentry.\n");
+            cart_print("turning off removeentry.\n");
             settings.remove_entry = false;
         } else if( strstr( word, "spoofpermissions" ) ){
-            printk("turning off spoofpermissions.\n");
+            cart_print("turning off spoofpermissions.\n");
             settings.spoof_permissions = false;
         } else if( strstr( word, "allsettings" ) ){
-            printk("turning off all settings.\n");
+            cart_print("turning off all settings.\n");
             settings.null_file = false;
             settings.remove_entry = false;
             settings.spoof_permissions = false;
         }
     } else {
-        printk("Unrecognized Option!\n");
+        cart_print("Unrecognized Option!\n");
         goto end;
     }
 
@@ -254,11 +247,30 @@ end:
 static struct ftrace_hook show_map_vma_hook;
 static struct file_operations file_ops;
 
+/* Search for a symbol in kallsyms. Some contain version specific suffixes */
+static int on_each_symbol(void *data, const char *name,
+                                      struct module *module, unsigned long address)
+{
+    if( strstr( name, "show_map_vma" ) != NULL ){
+        show_map_vma_hook.name = name;
+        show_map_vma_hook.address = address;
+        return 1; // non-zero stops iteration.
+    }
+
+    return 0;
+}
 static int cart_startup(void)
 {
-    printk("Cartographer Loaded.\n");
 
-    show_map_vma_hook.name = "show_map_vma";
+    if( !kallsyms_on_each_symbol( on_each_symbol, "show_map_vma" ) ){
+        cart_print( "Error iterating through modules!\n" );
+        return -EAGAIN;
+    }
+    if( !show_map_vma_hook.address ){
+        cart_print( "Error resolving the Address\n" );
+        return -ENXIO;
+    }
+    //show_map_vma_hook.name = "show_map_vma";
     show_map_vma_hook.function = cart_show_map_vma;
     show_map_vma_hook.original = &orig_show_map_vma;
     int ret = init_hook( &show_map_vma_hook );
@@ -273,33 +285,34 @@ static int cart_startup(void)
 
     ret = ftrace_set_filter_ip(&show_map_vma_hook.ops, show_map_vma_hook.address, 0, 0);
     if( ret ){
-        printk("ftrace_set_filter_ip() failed: %d\n", ret);
+        cart_print("ftrace_set_filter_ip() failed: %d\n", ret);
         return ret;
     }
 
     ret = register_ftrace_function(&show_map_vma_hook.ops);
     if (ret) {
-        printk("register_ftrace_function() failed: %d\n", ret);
+        cart_print("register_ftrace_function() failed: %d\n", ret);
         ftrace_set_filter_ip(&show_map_vma_hook.ops, show_map_vma_hook.address, 1, 0);
         return ret;
     }
 
     struct proc_dir_entry *entry = proc_create(PROC_FILENAME, 0, NULL, &file_ops);
     if( !entry ){
-        printk("proc_create failed! is NULL\nUnload and try again!\n");
+        cart_print("proc_create failed! is NULL\nUnload and try again!\n");
         return -EUCLEAN;
     }
 
     settings.target_lib = kmalloc(sizeof(char) * 64, GFP_KERNEL);
 
     if( !settings.target_lib ){
-        printk("Failed to allocate Memory for libname(init)\n");
+        cart_print("Failed to allocate Memory for libname(init)\n");
         return -ENOMEM;
     }
 
     file_ops.owner = THIS_MODULE;
     file_ops.write = on_write;
 
+    cart_print("Cartographer Loading complete.\n");
     return 0;
 }
 
@@ -308,11 +321,11 @@ static void cart_shutdown(void)
     int ret;
     ret = unregister_ftrace_function(&show_map_vma_hook.ops);
     if( ret )
-        printk("unregister_ftrace_function() failed: %d\n", ret);
+        cart_print("unregister_ftrace_function() failed: %d\n", ret);
 
     ret = ftrace_set_filter_ip(&show_map_vma_hook.ops, show_map_vma_hook.address, 1, 0);
     if( ret )
-        printk("ftrace_set_filter_ip() failed: %d\n", ret);
+        cart_print("ftrace_set_filter_ip() failed: %d\n", ret);
 
     remove_proc_entry(PROC_FILENAME, NULL);
 
@@ -320,7 +333,7 @@ static void cart_shutdown(void)
         kfree(settings.target_lib);
     }
 
-    printk("Cartographer UnLoaded.\n");
+    cart_print("Cartographer UnLoaded.\n");
 }
 
 module_init(cart_startup);
